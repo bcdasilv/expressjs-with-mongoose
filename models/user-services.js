06514fd84fa864e14f285f2be3b0f52be5dce0f8
Bruno da Silva <brunocarreiro@gmail.com>
2021-11-02 17:23:49 -0700

    Fixed code formatting issues

--- models/user-services.js
+++ models/user-services.js
@@ -1,60 +1,65 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userModel = require("./user");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-mongoose.connect(
-    "mongodb+srv://"+process.env.MONGO_USER+":"+process.env.MONGO_PWD+"@csc307.7ijdm.mongodb.net/"+process.env.MONGO_DB+"?retryWrites=true&w=majority",
+mongoose
+  .connect(
+    "mongodb+srv://" +
+      process.env.MONGO_USER +
+      ":" +
+      process.env.MONGO_PWD +
+      "@csc307.7ijdm.mongodb.net/" +
+      process.env.MONGO_DB +
+      "?retryWrites=true&w=majority",
     //'mongodb://localhost:27017/users',
     {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
     }
-).catch(error => console.log(error));
+  )
+  .catch((error) => console.log(error));
 
-async function getUsers(name, job){
-    let result;
-    if (name === undefined && job === undefined){
-        result = await userModel.find();
-    }
-    else if (name && (job === undefined)) {
-        result = await findUserByName(name);
-    }
-    else if (job && (name === undefined)){
-        result = await findUserByJob(job);
-    }
-    else {
-        result = await findUserByNameAndJob(name, job);
-    }    
-    return result;  
+async function getUsers(name, job) {
+  let result;
+  if (name === undefined && job === undefined) {
+    result = await userModel.find();
+  } else if (name && job === undefined) {
+    result = await findUserByName(name);
+  } else if (job && name === undefined) {
+    result = await findUserByJob(job);
+  } else {
+    result = await findUserByNameAndJob(name, job);
+  }
+  return result;
 }
 
-async function findUserById(id){
-    try{
-        return await userModel.findById(id);
-    }catch(error) {
-        console.log(error);
-        return undefined;
-    }
+async function findUserById(id) {
+  try {
+    return await userModel.findById(id);
+  } catch (error) {
+    console.log(error);
+    return undefined;
+  }
 }
 
-async function addUser(user){
-    try{
-        const userToAdd = new userModel(user);
-        const savedUser = await userToAdd.save()
-        return savedUser;
-    }catch(error) {
-        console.log(error);
-        return false;
-    }   
+async function addUser(user) {
+  try {
+    const userToAdd = new userModel(user);
+    const savedUser = await userToAdd.save();
+    return savedUser;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
-async function findUserByName(name){
-    return await userModel.find({'name':name});
+async function findUserByName(name) {
+  return await userModel.find({ name: name });
 }
 
-async function findUserByJob(job){
-    return await userModel.find({'job':job});
+async function findUserByJob(job) {
+  return await userModel.find({ job: job });
 }
 
 exports.getUsers = getUsers;
